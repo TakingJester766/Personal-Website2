b56484bfca678d3dd8ff87a32273042894a26aea
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-07-19 23:55:36 -0400

    alright I'm never using vanilla CSS again

---
+++ .history/src/App_20230719235447.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [title, setTitle] = useState('Hello world!')
+  const [description, setDescription] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.')
+  const [placeholder, setPlaceholder] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.')
+  const [isOpen, setIsOpen] = useState(false);
+
+
+
+  return (
+    <div>
+      <header className="text-center bg-blue-500 text-white py-4">
+        <h1 className="text-xl font-bold">Personal Site</h1>
+      </header>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+      <div className="flex-shrink-0">
+        <img className="h-12 w-12" src="/img/logo.svg" alt="Logo" />
+      </div>
+      <div className="ml-6 pt-1">
+        <h4 className="text-xl text-gray-900 leading-tight">{title}</h4>
+        <p className="text-base text-gray-600 leading-normal">{description}</p>
+      </div>
+    </div>
+
+    <input 
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+      type="text" 
+      placeholder={placeholder}
+    />
+          
+    </div>
+  )
+}
+
+export default App

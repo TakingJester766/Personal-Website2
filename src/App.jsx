ff1afa09207421f28dc29fd6ac0321ba31f7d1c8
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-09-22 23:27:11 -0400

    quick commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,52 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [title, setTitle] = useState('Hello world!')
-  const [description, setDescription] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.')
-  const [placeholder, setPlaceholder] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.')
-  const [isOpen, setIsOpen] = useState(false);
+  
 
 
 
   return (
     <div>
       <header className="text-center bg-blue-500 text-white py-4">
-        <h1 className="text-xl font-bold">Personal Site</h1>
+        <h1 className="text-xl font-bold">Alex Zimmerman</h1>
       </header>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
 
-      <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-      <div className="flex-shrink-0">
-        <img className="h-12 w-12" src="/img/logo.svg" alt="Logo" />
-      </div>
-      <div className="ml-6 pt-1">
-        <h4 className="text-xl text-gray-900 leading-tight">{title}</h4>
-        <p className="text-base text-gray-600 leading-normal">{description}</p>
-      </div>
-    </div>
+        <div className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+          <h1 className='text-6xl font-bold'>
+            Welcome to my <span className='text-blue-600'>Personal Site</span>
+          </h1>
 
-    <input 
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-      type="text" 
-      placeholder={placeholder}
-    />
+          <div className='flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full'>
+            <a
+              href='https://reactjs.org/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'
 
-(
-    <div className="h-screen flex overflow-hidden bg-gray-100">
-      <div className={`
-        ${isOpen ? 'block' : 'hidden'}
-        md:hidden
-      `}>
-        <div className="fixed inset-0 flex z-40">
-          <div className="fixed inset-0">
-            <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
+            >
+
+              <h3 className='text-2xl font-bold'>Check out what I'm about &rarr;</h3>
+
+              <p className='mt-4 text-xl'>
+                Ambission, drive, and a passion for learning
+              </p>
+
+            </a>
+
           </div>
-          <div className="
-            relative flex-1 flex flex-col max-w-xs w-full bg-white
-            transform transition-transform ease-in-out duration-300
-            ${isOpen ? 'translate-x-0' : '-translate-x-full'}
-          ">
-            <div className="absolute top-0 right-0 -mr-14 p-1">
-              <button 
-                className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600" 
-                aria-label="Close sidebar"
-                onClick={() => setIsOpen(false)}
-              >
-                <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-            <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-              <div className="flex-shrink-0 flex items-center px-4">
-                <img className="h-8 w-auto" src="/img/logos/workflow-logo-on-white.svg" alt="Workflow" />
-              </div>
-              {/* Menu content goes here */}
-            </div>
-          </div>
-        </div>
+          
+        
       </div>
-      <div className="flex flex-col w-0 flex-1 overflow-hidden">
-        <div className="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
-          <button 
-            className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:bg-gray-100 focus:text-gray-600 md:hidden" 
-            aria-label="Open sidebar"
-            onClick={() => setIsOpen(true)}
-          >
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-          {/* Rest of the page content goes here */}
-        </div>
+
       </div>
-    </div>
+
+      
           
     </div>
   )
